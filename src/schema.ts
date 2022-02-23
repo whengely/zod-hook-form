8c2982823f0e84ca238062dd041c6b9b1e03a39a
Walt Hengely <whengely@igsenergy.com>
2022-02-23 10:46:27 -0700

    add example of proper typing

--- src/schema.ts
+++ src/schema.ts
@@ -5,11 +5,24 @@ const personSchema = z.object({
   firstName: z.string().nonempty(),
   lastName: z.string().nonempty(),
 })
+type PersonSchema = z.infer<typeof personSchema>
 
 const organizationSchema = z.object({
   type: z.literal('Organization'),
   organizationName: z.string().nonempty()
 })
+type OrganizationSchema = z.infer<typeof organizationSchema>
 
 export const formSchema = z.union([personSchema, organizationSchema])
 export type FormSchema = z.infer<typeof formSchema>
+
+export const person: PersonSchema = { 
+  type: 'Person',
+  firstName: 'first',
+  lastName: 'last',
+}
+
+export const org: OrganizationSchema = {
+  type: 'Organization',
+  organizationName: 'org name'
+}
